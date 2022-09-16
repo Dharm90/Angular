32af8a3339c59056efde9c1292034fa7379c3015
Dharm90 <singhdha@ap.dtcs.local>
2022-09-16 11:08:26 +0530

    25th commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MyLibModule } from 'my-lib';
 
@@ -14,6 +14,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     MyLibModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
